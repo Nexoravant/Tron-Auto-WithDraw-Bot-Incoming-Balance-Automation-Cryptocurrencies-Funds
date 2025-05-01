6be2f268a300c96511d0e29ba5bfc1874204a2b3
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:51:51 +0300

    tronUi.cs

---
+++ tron/TronWithDraw/tronUi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankXor
+{
+    public class BankAccount
+    {
+        public string AccountName { get; set; }
+        public string Password { get; set; }
+        public decimal Balance { get; private set; }
+
+        public BankAccount(string accountName, string password, decimal balance)
+        {
+            AccountName = accountName;
+            Password = password;
+            Balance = balance;
+        }
+
+        public bool VerifyPassword(string password)
+        {
+            return Password == password;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public bool WithDraw(decimal amount)
+        {
+            if(amount <= Balance)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
